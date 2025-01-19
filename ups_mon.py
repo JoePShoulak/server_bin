d95dd1b4d406757c5ab176c0b30de6f5d6c7ee9c
Joe P Shoulak <1021647+JoePShoulak@users.noreply.github.com>
2025-01-19 00:23:21 -0600

    update

--- ups_mon.py
+++ ups_mon.py
@@ -1,33 +1,12 @@
-from os import system as sys
-from time import sleep
-import re
-
-# while True:
-#     sys("upsc apcups@localhost")
-#     sleep(5)
-
-
-# # Specify the file and the pattern
-# file_path = "example.txt"
-# pattern = r"hello"
-
-# # Open the file and search for the pattern
-# with open(file_path, "r") as file:
-#     for line in file:
-#         if re.search(pattern, line):
-#             print(line.strip())
-
-
-
-
 import subprocess
 
-command = ["upsc", "apcups@localhost"]
-pattern = "ups.status"
+def query_ups():
+    return subprocess.run(["upsc", "apcups@localhost"], capture_output=True, text=True)
 
-# Run and filter the output
-result = subprocess.run(command, capture_output=True, text=True)
-lines = [line for line in result.stdout.splitlines() if pattern in line]
+def get_ups_state():
+    pass
+
+lines = [line for line in query_ups().stdout.splitlines() if "ups.status" in line]
 
 for line in lines:
     print(line)
