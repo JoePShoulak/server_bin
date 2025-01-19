0703802193ef1d72395c1db748d29f063a90470f
Joe P Shoulak <1021647+JoePShoulak@users.noreply.github.com>
2025-01-19 00:26:41 -0600

    update

--- ups_mon.py
+++ ups_mon.py
@@ -4,11 +4,9 @@ def query_ups():
     return subprocess.run(["upsc", "apcups@localhost"], capture_output=True, text=True)
 
 def get_ups_state():
-    pass
+    status_line = [line for line in query_ups().stdout.splitlines() if "ups.status" in line][0]
+    print(status_line)
 
-lines = [line for line in query_ups().stdout.splitlines() if "ups.status" in line]
 
-for line in lines:
-    print(line)
-
-subprocess.run(["beep", "-f", "1000", "-r", "5", "-d", "100"])
+# subprocess.run(["beep", "-f", "1000", "-r", "5", "-d", "100"])
+get_ups_state()
