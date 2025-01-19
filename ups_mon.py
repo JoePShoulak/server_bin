69f544a307a4e8f710503c087e91c0b2fc337eb4
Joe P Shoulak <1021647+JoePShoulak@users.noreply.github.com>
2025-01-19 00:19:56 -0600

    update

--- ups_mon.py
+++ ups_mon.py
@@ -1,6 +1,33 @@
 from os import system as sys
 from time import sleep
+import re
 
-while True:
-    sys("upsc apcups@localhost")
-    sleep(5)
+# while True:
+#     sys("upsc apcups@localhost")
+#     sleep(5)
+
+
+# # Specify the file and the pattern
+# file_path = "example.txt"
+# pattern = r"hello"
+
+# # Open the file and search for the pattern
+# with open(file_path, "r") as file:
+#     for line in file:
+#         if re.search(pattern, line):
+#             print(line.strip())
+
+
+
+
+import subprocess
+
+command = ["upsc", "apcups@localhost"]
+pattern = "ups.status"
+
+# Run and filter the output
+result = subprocess.run(command, capture_output=True, text=True)
+lines = [line for line in result.stdout.splitlines() if pattern in line]
+
+for line in lines:
+    print(line)
