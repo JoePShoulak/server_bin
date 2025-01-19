270a6947c3b586ef491485c5967c4a2817574acb
Joe P Shoulak <1021647+JoePShoulak@users.noreply.github.com>
2025-01-19 01:29:29 -0600

    update

--- ups_mon.py
+++ ups_mon.py
@@ -2,9 +2,7 @@ import subprocess
 from beep import *
 from enum import Enum
 
-LOW_BATTERY_WARNING = """NOTE: The server computer has lost power and is running on battery.
-Your game server may shut down within 5 minutes. 
-"""
+LOW_BATTERY_WARNING = "NOTE: The server computer has lost power and is running on battery. Your game server may shut down within 5 minutes."
 
 class State(Enum):
     ONLINE = 2
