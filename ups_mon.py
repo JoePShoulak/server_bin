725a8cdc1161520534b6e8baf86feb5bcac757e7
Joe P Shoulak <1021647+JoePShoulak@users.noreply.github.com>
2025-01-19 00:32:23 -0600

    update

--- ups_mon.py
+++ ups_mon.py
@@ -1,13 +1,28 @@
 import subprocess
 
+from enum import Enum
+class State(Enum):
+    ONLINE = 2
+    BATTERY = 1
+    CRITICAL = 0
+    UNKNOWN = -1
+
 def query_ups():
     return subprocess.run(["upsc", "apcups@localhost"], capture_output=True, text=True)
 
 def get_ups_state():
     status_line = [line for line in query_ups().stdout.splitlines() if "ups.status" in line][0]
     status = status_line.split(": ")[-1].split(" ")
-    print(status)
+
+    if "LB" in status and "OB" in status: # Low Battery and On Battery
+        return State.CRITICAL
+    elif "OB" in status: # On Battery (but not critical)
+        return State.BATTERY
+    elif "OL" in status: # OnLine
+        return State.ONLINE
+    else:
+        return State.UNKNOWN
 
 
 # subprocess.run(["beep", "-f", "1000", "-r", "5", "-d", "100"])
-get_ups_state()
+print(get_ups_state())
