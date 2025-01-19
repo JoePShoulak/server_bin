0f5d46bf51cb779cd1bd1df73fb8deb0127fab08
Joe P Shoulak <1021647+JoePShoulak@users.noreply.github.com>
2025-01-19 16:10:55 -0600

    update

--- beep.py
+++ beep.py
@@ -5,6 +5,7 @@ def beep(freq=1000, repeat=1, delay=100):
   subprocess.run(["beep", "-f", str(freq), "-r", str(repeat), "-d", str(delay)])
 
 if __name__ == "__main__":
+  print("ran")
   beep(1000)
   sleep(1)
   beep(2000)
