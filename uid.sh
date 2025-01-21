0e4c2b482d158771726cec3de4322bfa2bbf3212
Joe P Shoulak <1021647+JoePShoulak@users.noreply.github.com>
2025-01-20 18:25:08 -0600

    update

--- uid.sh
+++ uid.sh
@@ -3,25 +3,25 @@
 LAST=''
 while true
 do
-    read -r _UID </opt/uid/uid
-    UID="${_UID^^}"
+    read -r STATE </opt/uid/uid
+    STATE="${STATE^^}"
 
-    if [ "$UID" == IDLE ]
+    if [ "$STATE" == IDLE ]
     then
-      [ "$LAST" == "$UID" ] && continue
+      [ "$LAST" == "$STATE" ] && continue
       ilo hp2 UID OFF
-    elif [ "$UID" == RUNNING ]
+    elif [ "$STATE" == RUNNING ]
     then
-      [ "$LAST" == "$UID" ] && continue
+      [ "$LAST" == "$STATE" ] && continue
       ilo hp2 UID ON
-    elif [ "$UID" == IN_USE ]
+    elif [ "$STATE" == IN_USE ]
     then
       ilo hp2 UID OFF
       sleep 5
       ilo hp2 UID ON
     fi
 
-    LAST="$UID"
+    LAST="$STATE"
 
     sleep 5
 done
